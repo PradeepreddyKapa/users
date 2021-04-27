f78190a77df5a76598ae499e48d55a7aa1cb0202
PradeepreddyKapa <82633584+PradeepreddyKapa@users.noreply.github.com>
2021-04-27 17:51:38 +0530

    Update UsersApiApplication.java

--- src/main/java/com/elgris/usersapi/UsersApiApplication.java
+++ src/main/java/com/elgris/usersapi/UsersApiApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
+@EnableScheduling
 public class UsersApiApplication {
 
 	public static void main(String[] args) {
